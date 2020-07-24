31e512989ca9c98b5be822b4eced40c73621dfa1
GregoryChocolate <47292994+GregoryChocolate@users.noreply.github.com>
2020-07-24 20:32:33 +0300

    CSS changes

--- js/renderLatestsPosts.js
+++ js/renderLatestsPosts.js
@@ -1,25 +1,5 @@
-// import Slider from './helpers/slider.js';
 import { ApiClient } from './helpers/api-client.js';
 import { LatestPost } from './components/LatestPost.js';
-
-// const slider1Config = {
-//   slidesContainer: document.querySelector('.portfolio__slider'),
-//   nextControl: document.querySelector('.portfolio-controllers__arrow--next'),
-//   prevControl: document.querySelector('.portfolio-controllers__arrow--prev'),
-//   slideShowDelay: 5000
-// };
-//
-// const slider1 = new Slider(slider1Config);
-
-// const slider2Config = {
-//   slidesContainer: document.querySelector('.testimonials__slider'),
-//   nextControl: document.querySelector('.testimonials__arrow--right'),
-//   prevControl: document.querySelector('.testimonials__arrow--left'),
-//   slideShow: false,
-//   moving: false
-// };
-
-// const slider2 = new Slider(slider2Config);
 
 const apiClient = new ApiClient();
 apiClient.getAllPosts()
